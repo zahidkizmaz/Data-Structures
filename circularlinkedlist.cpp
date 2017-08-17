360a71461dde1342f7684ff7f9ee393f2df4b9a7
Zahid KIZMAZ <mzkizmaz@gmail.com>
2017-08-17 20:58:05 +0300

    linkedlist search function added, circular linkedlist accessing first element by last element buggit add -A

--- circularlinkedlist.cpp
+++ circularlinkedlist.cpp
@@ -9,40 +9,47 @@ struct node{
 typedef node n;
 
 void printList(n *rootElement){
-	while(rootElement != NULL){
-		printf("%d \n",rootElement->data);
-		rootElement = rootElement->next;
+	n* iter = rootElement;
+	do{
+		printf("%d \n",iter->data);
+		iter = iter->next;
 	}
+	while(iter->next != rootElement);
+
 }
 
 n* createLinkedList(int data){
 	n* element;
 	element = (n *) malloc(sizeof(n));
-	element->next= NULL;
+	element->next= element;
 	element->data= data;
 	return element;
 }
 
-void add(n *element, int data){
-	while(element->next != NULL){
-		element = element->next;
+void add(n *root, int data){
+	n* iter = root;
+	while(iter->next != root){
+		iter = iter->next;
 	}
-	element->next = (n *)malloc(sizeof(n));
-	element = element->next;
-	element->data = data;
-	element->next = NULL;
+	iter->next = (n *)malloc(sizeof(n));
+	iter = iter->next;
+	iter->data = data;
+	iter->next = root;
 }
 
 n* remove(n* root,int data){
 	n* iter = root;
 	if(data == iter->data){
 		n* temp = iter;
-		iter = iter->next;
-	       	free(temp);	
-		return iter;
+		while(temp->next != root){
+			temp=temp->next;
+		}
+		temp->next=root->next;
+	       	free(root);	
+		return temp->next;
 	}
 	else{
-		while(iter->next != NULL){
+		while(iter->next != root){
 			if(iter->next->data == data){
 				n* temp = iter->next;
 				iter->next = temp->next;
@@ -57,11 +64,12 @@ n* remove(n* root,int data){
 }
 
 n* search(n* root, int data){
-	while(root->next != NULL){
-		if(root->data == data){
-			return root;
+	n* iter = root;
+	while(iter->next != root){
+		if(iter->data == data){
+			return iter;
 		}
-		root=root->next;
+		iter=iter->next;
 	}
 	return NULL;
 }
@@ -71,14 +79,14 @@ int main(){
 	n * root = createLinkedList(1);
 	add(root, 2);
 	add(root, 3);
-	add(root, 4);
 	add(root, 5);
 	printList(root);
 	printf("\n");
-	root = remove(root,1);
 	printList(root);
 	root = remove(root,7);
-	printf("\n%d\n",search(root,3)->data);
-
+	//printf("\n%d\n",search(root,5)->next->data);
+	n* last = search(root,5);
+	last= last->next;
+	//printf("  %d  ",last->data );
 	return 0;
 }
